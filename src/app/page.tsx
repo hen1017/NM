473d129c0bb732fb657770fef1fc25f2440416b3
hankchen <hankchen@yahoo.com>
2025-07-15 16:01:41 +1200

    Replace default homepage with custom one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>Welcome to NM Blog!</h1>
       <p>這是NM自架的部落格網站 </p>
-      <a href="/admin/login">前往後台登入</a>
+     <p><a href="/admin/login">前往後台登入</a></p>
     </main>
   );
 }
