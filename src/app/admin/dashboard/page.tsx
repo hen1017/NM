4be8ff8db13b20d9a37d23c4ffa2b03c965d2b07
hankchen <hankchen@yahoo.com>
2025-07-17 23:23:17 +1200

    Upgrade next-auth to fix getServerSession

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from '@/lib/authOptions';
+import { authOptions } from "@/lib/authOptions";
 import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
